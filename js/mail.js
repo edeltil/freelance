6470a24d43e429c0ec110045c68217b1a90e19fc
edeltil <edeltil@gmail.com>
2015-10-15 23:10:55 +0200

    Site updated at 2015-10-15 21:10:55 UTC

---
+++ js/mail.js
@@ -0,0 +1,39 @@
+$(document).ready(function($) {
+    $('#contactForm').submit(function (e) {
+        e.preventDefault();
+        // get values from FORM
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+        var webSite = $("input#webSite").val();
+        var budget = $("#budget option:selected" ).text();
+        var delai = $("#delai option:selected" ).text();
+        var message = $("textarea#message").val();
+
+        Parse.initialize("tsVjWn4AwCdEkUkXR1dmuqaAMogtYspBBlHTWNRw", "Xm3zcKBgHkDpprIoTZm5H04BNe5GP6TalznNXuhm");
+        
+        var mail = "Un nouveau client "+name+" a effectué une demande : \n"+
+        "Son ancien site est : "+webSite+".\n"+
+        "Son budget est de : "+budget+".\n"+
+        "Il souhaite que son projet soit réalisé dans un delai de "+delai+".\n"+
+        "Voici le détail de son projet : \n"+message+".\n"+
+        "Voici son mail pour le contacter : "+email+".\n";
+
+        Parse.Cloud.run('mailSend', {
+            target: 'edeltil@gmail.com',
+            originator: email,
+            subject : '[Freelance] Nouveau client : '+name,
+            text :mail
+        }, {
+            // Success handler
+            success: function() {
+                $('#sendMail').show();
+                $('#contactForm')[0].reset();
+            },
+            // Error handler
+            error: function(message) {
+                alert('Error: ' + message);
+            }
+        });
+
+    });
+});
